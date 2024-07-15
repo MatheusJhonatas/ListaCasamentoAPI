d4d52536be46f97d8c4287357798faac216ea641
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2024-07-15 20:04:27 -0300

    Add Class padrinho and Convidado

---
+++ Models/Convidado.cs
@@ -0,0 +1,7 @@
+using System;
+namespace Models.Pessoa;
+
+public class Convidado : Pessoa
+{
+    public bool Confirmacao { get; set; }
+}
---
+++ Models/Padrinho.cs
@@ -0,0 +1,7 @@
+using System;
+namespace Models.Pessoa;
+
+public class Padrinho : Pessoa
+{
+
+}
