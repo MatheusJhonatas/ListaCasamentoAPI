6d6a79ff3a944406d483846c0c771414dff82eff
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2024-08-12 17:36:18 -0300

    finish endpoints

--- Controllers/ConvidadosController.cs
+++ Controllers/ConvidadosController.cs
@@ -46,5 +46,90 @@ namespace Controllers
                 return StatusCode(500, new ResultViewModel<Convidado>("15XCD -Falha Interna no Servidor"));
             }
         }
+        [HttpPost("v1/convidados")]
+        public async Task<IActionResult> PutAsync(
+            [FromBody] EditorPessoaViewModel model,
+            [FromServices] ListaCasamentoDataContext context
+        )
+        {
+            try
+            {
+                var convidado = new Convidado
+                {
+                    Nome = model.Nome,
+                    Aniversario = model.Aniversario,
+                    Sexo = model.Sexo,
+                    Familia = model.Familia,
+                    Telefone = model.Telefone,
+                    Email = model.Email.ToLower(),
+                    Id = Guid.NewGuid()
+                };
+                await context.Convidados.AddAsync(convidado);
+                await context.SaveChangesAsync();
+                return Created($"v1/convidados{convidado.Id}", new ResultViewModel<Convidado>(convidado));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Convidado>("16XCD -Falha Interna no Servidor"));
+            }
+        }
+
+        [HttpDelete("v1/convidados/{id:Guid}")]
+        public async Task<IActionResult> DeleteAsync(
+            [FromRoute] Guid id,
+            [FromServices] ListaCasamentoDataContext context
+        )
+        {
+            try
+            {
+                var deletarConvidado = context.Convidados.FirstOrDefault(c => c.Id == id);
+                if (deletarConvidado == null)
+                {
+                    return NotFound(new ResultViewModel<Convidado>("4XCD - ID do convidado não encontrado"));
+                }
+                context.Convidados.Remove(deletarConvidado);
+                await context.SaveChangesAsync();
+                return Ok(new ResultViewModel<Convidado>(deletarConvidado + $"Usuário {deletarConvidado.Nome} foi excluido com sucesso"));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Convidado>("17XCD - Falha interna no servidor"));
+            }
+        }
+
+
+        [HttpPut("v1/convidados/{id:Guid}")]
+        public async Task<IActionResult> PutAsync(
+            [FromRoute] Guid id,
+            [FromBody] EditorPessoaViewModel model,
+            [FromServices] ListaCasamentoDataContext context
+        )
+        {
+            try
+            {
+                var atualizaConvidado = await context.Convidados.FirstOrDefaultAsync(c => c.Id == id);
+                if (atualizaConvidado == null)
+                {
+                    return NotFound();
+                }
+
+                atualizaConvidado.Nome = model.Nome;
+                atualizaConvidado.Aniversario = model.Aniversario;
+                atualizaConvidado.Sexo = model.Sexo;
+                atualizaConvidado.Familia = model.Familia;
+                atualizaConvidado.Telefone = model.Telefone;
+                atualizaConvidado.Email = model.Email.ToLower();
+
+
+                context.Update(atualizaConvidado);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<Convidado>(atualizaConvidado));
+            }
+            catch
+            {
+                return StatusCode(500, new ResultViewModel<Convidado>("18XCD - Falha Interna no Servidor"));
+            }
+        }
     }
 }
